08a13a374e414b3c71d2c92da8035be244009b58
WangLiang <wl@lookmap.net>
2021-02-02 18:51:34 +0800

    优化

--- android/src/main/kotlin/com/chuangdun/flutter/plugin/JyIDReader/JyIDReaderPlugin.kt
+++ android/src/main/kotlin/com/chuangdun/flutter/plugin/JyIDReader/JyIDReaderPlugin.kt
@@ -16,113 +16,131 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.io.ByteArrayOutputStream
-import java.lang.ref.WeakReference
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 private const val TAG = "JyIDReaderPlugin"
+
 /** JyIDReaderPlugin */
-class JyIDReaderPlugin: FlutterPlugin{
+class JyIDReaderPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler{
 
-  private lateinit var channel : MethodChannel
-  private lateinit var eventChannel : EventChannel
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "JyIDReader")
-    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "JyIDReader/onRead")
-    val readerHandler = IDReaderHandler(flutterPluginBinding.applicationContext)
-    channel.setMethodCallHandler(readerHandler)
-    eventChannel.setStreamHandler(readerHandler)
-  }
-
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
-
-  class IDReaderHandler(context: Context): MethodCallHandler, EventChannel.StreamHandler{
-    private var mediaPlayer: MediaPlayer?=null
-    private val tag = this.javaClass.simpleName
-    private val contextRef = WeakReference<Context>(context)
+    private val threadPool = ThreadPoolExecutor(
+            1, 1, 0L, TimeUnit.MILLISECONDS,
+            LinkedBlockingQueue<Runnable>())
     private val mainHandler = Handler()
-    private val listener: IDCardRead.getIDCardInfoListen = IDCardRead.getIDCardInfoListen(){ type: Int,//证件类型
-                                                                                             idCardInfo: IDCardInfo?,//中国大陆居民身份证
-                                                                                             idprpCardInfo: IDPRPCardInfo?,//外国人永居证
-                                                                                             gangaotaiIdCardInfo: IDCardInfo?,//港澳台居住证
-            // 身份证头像
-                                                                                             bitmap: Bitmap ->
-      run {
+    private var events: EventChannel.EventSink? = null
+    private var mMediaPlayer:MediaPlayer? = null
+    private lateinit var context: Context
+    private lateinit var mIDCardRead: IDCardRead
+    private lateinit var methodChannel: MethodChannel
+    private lateinit var eventChannel: EventChannel
 
-        when (type) {
-          1 -> {
-            mediaPlayer = MediaPlayer.create(contextRef.get(), R.raw.ic_card_success)
-            mediaPlayer?.start()
-            val data = mutableMapOf<String, Any>()
-            data["type"] = 1
-            data["id"] = idCardInfo!!.id
-            data["name"] = idCardInfo!!.name
-            data["sex"] = idCardInfo!!.sex
-            data["nation"] = idCardInfo!!.nation
-            data["birth"] = idCardInfo!!.birth
-            data["address"] = idCardInfo!!.address
-            data["depart"] = idCardInfo!!.depart
-            data["validityTime"] = idCardInfo!!.validityTime
-            val outputStream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            data["bitmap"] = outputStream.toByteArray()
-            mainHandler.post { events?.success(data) }
+    private val listener: IDCardRead.getIDCardInfoListen =
+            IDCardRead.getIDCardInfoListen() { type: Int,//证件类型
+                                               idCardInfo: IDCardInfo?,//中国大陆居民身份证
+                                               _: IDPRPCardInfo?,//外国人永居证
+                                               _: IDCardInfo?,//港澳台居住证
+                                               bitmap: Bitmap ->     // 身份证头像
+                run {
 
-          }
+                    when (type) {
+                        1 -> {
+                            playSound(R.raw.ic_card_success, 1800)
+                            val data = mutableMapOf<String, Any>()
+                            data["type"] = 1
+                            data["id"] = idCardInfo!!.id
+                            data["name"] = idCardInfo!!.name
+                            data["sex"] = idCardInfo!!.sex
+                            data["nation"] = idCardInfo!!.nation
+                            data["birth"] = idCardInfo!!.birth
+                            data["address"] = idCardInfo!!.address
+                            data["depart"] = idCardInfo!!.depart
+                            data["validityTime"] = idCardInfo!!.validityTime
+                            val outputStream = ByteArrayOutputStream()
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                            data["bitmap"] = outputStream.toByteArray()
+                            mainHandler.post { events?.success(data) }
+
+                        }
 //          2 -> Log.w(tag, "读取到外国人居住证")
 //          3 -> Log.w(tag, "读取到港澳台居住证")
-          else -> {
-            mediaPlayer = MediaPlayer.create(contextRef.get(), R.raw.ic_card_failure)
-            mediaPlayer?.start()
-            Log.w(tag, "读取到未知的证件类型")
-          }
-        }
-      }
+                        else -> {
+                            Log.w(TAG, "读取到未知的证件类型")
+                            playSound(R.raw.ic_card_failure, 1800)
+                        }
+                    }
+                }
+            }
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+        methodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "JyIDReader")
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "JyIDReader/onRead")
+        methodChannel.setMethodCallHandler(this)
+        eventChannel.setStreamHandler(this)
     }
-    private var events: EventChannel.EventSink? = null
-    private lateinit var mIDCardRead:IDCardRead
+
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        methodChannel.setMethodCallHandler(null)
+        if (!threadPool.isShutdown){
+            threadPool.shutdownNow()
+        }
+    }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
-      Log.i(tag, "身份证阅读器调用:${call.method}, 参数:${call.arguments}")
-
-      when(call.method){
-        "init" -> {
-          val arguments = call.arguments as Map<*, *>
-          mIDCardRead = IDCardRead(contextRef.get())
-          mIDCardRead.setUSBTimeOut(arguments["usbTimeout"] as Int)
-          mIDCardRead.setCycle(arguments["cycle"] as Int)
-          mIDCardRead.setIDCardLister(listener)
+        Log.i(TAG, "身份证阅读器调用:${call.method}, 参数:${call.arguments}")
+        when (call.method) {
+            "init" -> {
+                val arguments = call.arguments as Map<*, *>
+                mIDCardRead = IDCardRead(context)
+                mIDCardRead.setUSBTimeOut(arguments["usbTimeout"] as Int)
+                mIDCardRead.setCycle(arguments["cycle"] as Int)
+                mIDCardRead.setIDCardLister(listener)
+            }
+            "openDevice" -> {
+                result.success(mIDCardRead.openDevice())
+            }
+            "getSAMID" -> {
+                result.success(mIDCardRead.samid)
+            }
+            "startIDThread" -> {
+                result.success(true)
+                threadPool.execute{
+                    playSound(R.raw.ic_card_began_read, 3000)
+                    mIDCardRead.startIDThread()
+                }
+            }
+            "stopIDThread" -> {
+                mIDCardRead.stopIDThread()
+            }
+            "closeDevice" -> {
+                result.success(mIDCardRead.closeDevice())
+            }
+            else -> result.notImplemented()
         }
-        "openDevice" -> {
-          result.success(mIDCardRead.openDevice())
-        }
-        "getSAMID" -> {
-          result.success(mIDCardRead.samid)
-        }
-        "startIDThread" -> {
-          mediaPlayer = MediaPlayer.create(contextRef.get(), R.raw.ic_card_began_read)
-          mediaPlayer?.start()
-          result.success(mIDCardRead.startIDThread())
-        }
-        "stopIDThread" -> {
-          mIDCardRead.stopIDThread()
-        }
-        "closeDevice" -> {
-          mediaPlayer?.stop()
-          mediaPlayer?.release()
-          result.success(mIDCardRead.closeDevice())
-        }
-        else -> result.notImplemented()
-      }
     }
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-      this.events = events
+        this.events = events
     }
 
     override fun onCancel(arguments: Any?) {
-      this.events = null
+        this.events = null
     }
-  }
+
+    private fun playSound(resid: Int, waitMillis: Long){
+        try {
+            mMediaPlayer = MediaPlayer.create(context, resid)
+            mMediaPlayer!!.start()
+            Thread.sleep(waitMillis)
+            mMediaPlayer!!.stop()
+            mMediaPlayer!!.release()
+        }catch (e: InterruptedException){
+            Log.e(TAG, "线程睡眠waitMillis毫秒失败.${e.message}")
+        }catch (e: Exception) {
+            Log.e(TAG, "MediaPlayer错误.${e.message}")
+        }
+    }
 }
